89c2b86f646d64758211fbfb47cceec9db17a298
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-05-03 15:20:50 +0200

    content component now spawns 3 Part components of its own

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import React from 'react'
 const Header = (props) => {
   return <h1>{props.course} </h1>
 }
- 
-const Content = (props) => {
-  return (
-    <p>
-      {props.part} {props.excercise}
-    </p>
-  )
+
+const Part = (props) => {
+  return <p>
+    {props.part} {props.excercise}
+  </p>
 }
 
 const Total = (props) => {
   return <p>Number of excercises {props.total}</p>
 } 
-
-const App = () => {
-  const course = 'Half Stack application development'
+ 
+const Content = (props) => {
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -27,14 +24,23 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-      <Content part={part1} excercise={exercises1} />
-      <Content part={part2} excercise={exercises2} />
-      <Content part={part3} excercise={exercises3} />
+      <Part part={part1} excercise={exercises1} />
+      <Part part={part2} excercise={exercises2} />
+      <Part part={part3} excercise={exercises3} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
 
+const App = () => {
+  const course = 'Half Stack application development'
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+    </div>
+  )
+}
 
 export default App
