b24ffba06a45ad689c06b8ff2bf1129820372316
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-05-10 10:48:40 +0200

    initialize contact filter function

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', phone: 9498675309 }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '040-123456' },
+    { name: 'Ada Lovelace', phone: '39-44-5323523' },
+    { name: 'Dan Abramov', phone: '12-43-234345' },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
-  const [ newPhone, setNewPhone ] = useState()
+  const [ newPhone, setNewPhone ] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -13,6 +16,15 @@ const App = () => {
 
   const handlePhoneChange = (event) => {
     setNewPhone(event.target.value)
+  }
+
+  const handleContactFilter = (event) => {
+    // change to .filter? 
+    persons.forEach(person => {
+      if(person.name.toLowerCase().includes(event.target.value.toLowerCase())){
+        console.log(person.name)
+      }
+    })
   }
 
   const addContact = (event) => {
@@ -49,6 +61,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input onChange={handleContactFilter} />
+      <h2>add a new</h2>
       <form onSubmit={addContact}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
