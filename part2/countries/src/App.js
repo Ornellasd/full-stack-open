33fe36a4c1d436e72a043cecd8fc3fc597947746
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-05-11 13:35:46 +0200

    countries are filtered by search term

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+const Display = ({ countriesToShow }) => {
+  if(countriesToShow.length > 10) {
+    return <p>Too many matches, specify another filter</p>
+  } else {
+    return (
+      <div>
+        {countriesToShow.map(country =>
+          <p>{country.name}</p> 
+        )}
+      </div>
+    )
+  }
+}
+
 const App = () => {
   const [countries, setCountries] = useState([])
   const [searchTerm, setSearchTerm] = useState([''])
@@ -14,16 +28,30 @@ const App = () => {
 
   useEffect(fetchCountries, [])
 
+  const countriesToShow = countries.filter(country => {
+    return country.name.toLowerCase().includes(searchTerm)
+  })
+  
   const handleFilterChange = (event) => {
-    setSearchTerm(event.target.value)
+    setSearchTerm(event.target.value.toLowerCase())
   }
+
+  /*
+  <div>
+      find countries <input onChange={handleFilterChange} />
+      {if(countriesToShow.length > 10) {
+        return <p></p>
+      }}
+      {countriesToShow.map(country =>
+        <p>{country.name}</p> 
+      )}
+    </div>
+    */
 
   return (
     <div>
       find countries <input onChange={handleFilterChange} />
-      {countries.map(country =>
-        <p>{country.name}</p> 
-      )}
+      <Display countriesToShow={countriesToShow} />
     </div>
   )
 }
