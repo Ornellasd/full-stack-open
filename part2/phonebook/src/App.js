ef2cfecbd82681596f5df0d82e485b16a49239e0
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-05-11 10:54:40 +0200

    finish 2.11 - initial state set by db.json

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const ContactInput = ({ submit, name, phone, nameChange, phoneChange }) => {
   return (
@@ -23,15 +24,19 @@ const ContactDisplay = ({ contact }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456' },
-    { name: 'Ada Lovelace', phone: '39-44-5323523' },
-    { name: 'Dan Abramov', phone: '12-43-234345' },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newPhone, setNewPhone ] = useState('')
   const [ filter, setFilter ] = useState('')
+
+  const fetchNotes = () => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }
+
+  useEffect(fetchNotes, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
