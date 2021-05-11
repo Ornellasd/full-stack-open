1d1311f79a53b80e73d535c5d0d8dd4ad4159371
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-05-11 16:41:19 +0200

    initialize weather fetch

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,9 +2,22 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Display = ({ countriesToShow, setSearchTerm }) => {
+  const [weather, setWeather] = useState([])
+
+  const fetchWeather = (city) => {
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+      .then(response => {
+        console.log(response.data)
+        setWeather(response.data)
+      })
+  }
+
   if(countriesToShow.length > 10) {
     return <p>Too many matches, specify another filter</p>
   } else if(countriesToShow.length === 1) {
+    // hide search box to limit api calls
+    //fetchWeather(countriesToShow[0].capital)
+
     return (
       <div>
         <h1>{countriesToShow[0].name}</h1>
@@ -17,6 +30,10 @@ const Display = ({ countriesToShow, setSearchTerm }) => {
           )}
         </ul>
         <img src={countriesToShow[0].flag} width="150" />
+        <h2>Weather in {countriesToShow[0].capital}</h2>
+        <h3>temperature:</h3>
+        <img src="http://openweathermap.org/img/wn/03n@2x.png" />
+        <h3>wind:</h3>
       </div>
     )
   } else {
