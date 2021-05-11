1922056eebde88c98dbcadbafbffb0439e9a5513
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-05-11 12:01:35 +0200

    basic api fetch and display working

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,6 +2,30 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
+  const [countries, setCountries] = useState([])
+  const [searchTerm, setSearchTerm] = useState([''])
+  
+  const fetchCountries = () => {
+    axios.get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }
+
+  useEffect(fetchCountries, [])
+
+  const handleFilterChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  return (
+    <div>
+      find countries <input onChange={handleFilterChange} />
+      {countries.map(country =>
+        <p>{country.name}</p> 
+      )}
+    </div>
+  )
 }
 
 export default App
