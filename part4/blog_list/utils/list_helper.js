a32f935a5f25b9640b9ea3e3067926e2cb80ec17
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-05-31 17:13:51 +0200

    finish 4.5 - test for blog with most likes

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -1,25 +1,19 @@
-const listWithOneBlog = [
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-]
-
 const dummy = (blogs) => {
   return 1
 }
 
-const totalLikes = (array) => {
-  return array.length === 1
-    ? array[0].likes
-    : array.reduce((accum, item) => accum + item.likes, 0) 
+const totalLikes = (blogs) => {
+  return blogs.length === 1
+    ? blogs[0].likes
+    : blogs.reduce((accum, item) => accum + item.likes, 0) 
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog)
 }
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog
 }
