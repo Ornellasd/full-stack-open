f847603135b3c2886e69b7570a049d881bd9f653
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-05-31 12:43:47 +0200

    finish 4.2 - break out app starting logic into index.js

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -1,32 +1,10 @@
+const app = require('./app')
+const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogRouter = require('./controllers/blog')
-const middleware = require('./utils/middleware')
 
-const Blog = require('./models/blog')
+const server = http.createServer(app)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.use('/api/blogs', blogRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
