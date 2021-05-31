5a04dfa54422053ad7568231635d83596591286e
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-05-31 12:17:38 +0200

    break route handlers out into own module

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blog')
 require('dotenv').config()
 
 const Blog = require('./models/blog')
@@ -29,23 +30,7 @@ app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
