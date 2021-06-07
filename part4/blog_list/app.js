caf0af959bba96711ac650df25f67fe1029d9a02
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-06-07 17:24:01 +0200

    finish 4.18 - token based authentication implemented

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blog')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -23,6 +24,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
