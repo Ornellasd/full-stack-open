48d5a9b8c64626aab5f105c14de69c8879b781c9
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-06-07 11:14:19 +0200

    initialize user controler/model, user can be created

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -5,10 +5,9 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blog')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
-
-const Blog = require('./models/blog')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(() => {
@@ -23,6 +22,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
