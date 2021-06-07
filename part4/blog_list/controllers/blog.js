2db5c1163f83553f6172b7ad9b0b3d1a0ec53f88
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-06-07 18:12:44 +0200

    finish 4.19 - adding new blog only possible if valid token is sent with POST request

--- part4/blog_list/controllers/blog.js
+++ part4/blog_list/controllers/blog.js
@@ -1,6 +1,15 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -11,8 +20,16 @@ blogRouter.get('/', async (request, response) => {
 
 blogRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.find({})
+  console.log(body, "??????")
 
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  console.log(decodedToken)
+
+  if(!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   if(!body.title && !body.author) {
     return response.status(400).json({ error: 'title and author missing' })
   } else if(!body.likes) {
@@ -24,12 +41,12 @@ blogRouter.post('/', async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: user[0]._id
+    user: user._id
   })
   
   const savedBlog = await blog.save()
-  user[0].blogs = user[0].blogs.concat(savedBlog._id)
-  await user[0].save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
 
   response.json(savedBlog)
 })
