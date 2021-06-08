5ea6fe6148b2e72804ea61281bbf3c2703075081
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-06-08 11:40:32 +0200

    finish 4.21 - blog can only be deleted by user who added it

--- part4/blog_list/controllers/blog.js
+++ part4/blog_list/controllers/blog.js
@@ -46,13 +46,16 @@ blogRouter.get('/:id', async (request, response) => {
 
 blogRouter.delete('/:id', async (request, response) => {
   const decodedToken = jwt.verify(request.token, process.env.SECRET)
-
+  const blog = await Blog.findById(request.params.id)
+  
   if(!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
+  } else if(blog.user.toString() === decodedToken.id) {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } else {
+    return response.status(401).json({ error: `blog not owned by ${decodedToken.username}`})
   }
-  
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
 })
 
 blogRouter.put('/:id', async (request, response) => {
