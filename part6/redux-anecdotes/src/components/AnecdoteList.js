e8c4c0f886475c3589cf10a1b523e93c23d724ac
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-06-23 17:37:27 +0200

    finish 6.11 - implements filtering of anecdotes with respective reducer and action creator

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,9 +2,14 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { upvote } from '../reducers/anecdoteReducer'
 import { setNotification, clearNotification } from '../reducers/notificationReducer'
+import Filter from '../components/Filter'
 
 const AnecdoteList = () => {
   const anecdotes = useSelector(state => state.anecdotes)
+  const filter = useSelector(state => state.filter)
+
+  const anecdotesToShow = anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(filter))
+
   const dispatch = useDispatch()
   
   const vote = (id, content) => {
@@ -18,7 +23,8 @@ const AnecdoteList = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      <Filter />
+      {anecdotesToShow.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
