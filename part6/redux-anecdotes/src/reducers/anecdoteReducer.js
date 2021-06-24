271b909cd654a895ffb92f94a9fc49a0239b52b3
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-06-24 15:52:08 +0200

    finish 6.15 - redux store initialized using asynchronous action creators using redux-thunk

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import anecdoteService from '../services/anecdotes'
+
 const order = (a, b) => {
   return b.votes - a.votes
 }
@@ -38,10 +40,13 @@ export const createAnecdote = (content) => {
   }
 }
 
-export const initializeAnecdotes = (anecdotes) => {
-  return {
-    type: 'INIT_ANECDOTES',
-    data: anecdotes
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes,
+    })
   }
 }
 
