8fadd258ed3ade2f0bc4e4b630218145e66102c0
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-06-24 16:21:52 +0200

    finish 6.16 - creation of new anecdote happens using asychronous action creator, redux-thunk

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -33,10 +33,13 @@ export const upvote = (id) => {
   }
 }
 
-export const createAnecdote = (content) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    data: content
+export const createAnecdote = content => {
+  return async dispatch => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch({
+      type: 'NEW_ANECDOTE',
+      data: newAnecdote
+    })
   }
 }
 
