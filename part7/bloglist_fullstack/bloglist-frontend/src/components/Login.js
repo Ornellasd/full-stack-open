251dbc64816a7a9d9725c126d807ad266bd196f5
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-07-31 21:06:49 -0700

    apply login styling from material-ui template

--- part7/bloglist_fullstack/bloglist-frontend/src/components/Login.js
+++ part7/bloglist_fullstack/bloglist-frontend/src/components/Login.js
@@ -1,13 +1,37 @@
 import React from 'react'
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+
 import { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 
-import {
-  TextField,
-  Button
-} from '@material-ui/core'
-
 import { login } from '../reducers/loginReducer'
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}))
 
 const Login = ({ alerts }) => {
   const dispatch = useDispatch()
@@ -25,21 +49,50 @@ const Login = ({ alerts }) => {
     history.push('/')
   }
 
+  const classes = useStyles()
+
   return (
-    <div>
-      <h2>Log in to application</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <TextField label="username" name="username" />
-        </div>
-        <div>
-          <TextField label="password" type="password" name="password" />
-        </div>
-        <div>
-          <Button variant="contained" color="primary" type="submit">login</Button>
-        </div>
-      </form>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Log in to application
+        </Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <TextField 
+            label="Username"
+            name="username"
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            autoFocus
+          />
+          <TextField 
+            label="password" 
+            type="password" 
+            name="password"
+            variant="outlined"
+            margin="norma"
+            required
+            fullWidth
+            autoFocus 
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Log In
+          </Button>
+        </form>
+      </div>
+    </Container>
   )
 }
 
