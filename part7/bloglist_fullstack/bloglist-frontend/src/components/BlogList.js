f5b58fdeafc4340fe9769eb5cb71df542fc2e7df
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-08-03 15:50:28 -0700

    change styling off blog title in BlogList and make hoverable

--- part7/bloglist_fullstack/bloglist-frontend/src/components/BlogList.js
+++ part7/bloglist_fullstack/bloglist-frontend/src/components/BlogList.js
@@ -1,17 +1,29 @@
 import { Link } from 'react-router-dom'
 
-import { Divider, List, ListItem } from '@material-ui/core'
+import { Divider, List, ListItem, makeStyles } from '@material-ui/core'
 
 import BlogForm from '../components/BlogForm'
 
+const useStyles = makeStyles((theme) => ({
+  link: {
+    textDecoration: 'none',
+    color: '#000000'
+  },
+  article: {
+    lineHeight: 1.35
+  }
+}))
+
 const BlogList = ({ blogs }) => {
+  const classes = useStyles()
+
   return (
     <div>
       {blogs.map(blog =>
         <List>
-          <Link to={`/blogs/${blog.id}`}>
-            <ListItem>
-              <article>
+          <Link to={`/blogs/${blog.id}`} className={classes.link}>
+            <ListItem button>
+              <article className={classes.article}>
                 <h2>
                   <strong>{blog.title}</strong>
                 </h2>
