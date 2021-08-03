d20f24300c0d916bef142c8fe6967c645358718d
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-08-03 12:50:41 -0700

    initialize material-ui styling for BlogList container

--- part7/bloglist_fullstack/bloglist-frontend/src/components/BlogList.js
+++ part7/bloglist_fullstack/bloglist-frontend/src/components/BlogList.js
@@ -1,24 +1,25 @@
 import { Link } from 'react-router-dom'
 
+import { Divider, List, ListItem } from '@material-ui/core'
+
 import BlogForm from '../components/BlogForm'
 
 const BlogList = ({ blogs }) => {
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5
-  }
-
   return (
     <div>
-      <BlogForm />
-            
       {blogs.map(blog =>
-        <div style={blogStyle} className="unexpanded">
-          <Link to={`/blogs/${blog.id}`}>{blog.title} {blog.author}</Link>
-        </div>
+        <List>
+          <Link to={`/blogs/${blog.id}`}>
+            <ListItem>
+              <article>
+                <h2>
+                  <strong>{blog.title}</strong>
+                </h2>
+              </article>
+            </ListItem>
+          </Link>
+          <Divider />
+        </List>
       )}
     </div>
   )
