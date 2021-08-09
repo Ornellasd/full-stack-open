8d5419c8e888dc1501174dfcdef6e0e887e17d1e
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-08-09 16:27:21 -0700

    allBooks can be used to query books by specific genre

--- part8/library-backend/src/index.js
+++ part8/library-backend/src/index.js
@@ -107,7 +107,10 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(
+      author: String
+      genre: String
+    ): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -117,10 +120,13 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if(!args.author) {
+      if(!args) {
         return books
+      } else if(args.author) {
+        return books.filter(book => book.author === args.author)
+      } else {
+        return books.filter(book => book.genres.includes(args.genre))
       }
-      return books.filter(book => book.author === args.author)
     },
     allAuthors: () => authors
   },
