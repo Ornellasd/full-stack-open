646a6146a0ae938960a7d29ff6825c5ca0a78ae0
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-08-13 13:17:02 -0700

    initialize ALL_AUTHORS query

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,11 +1,29 @@
-
 import React, { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      id
+      born
+      bookCount
+    }
+  }
+`
 const App = () => {
   const [page, setPage] = useState('authors')
+  const result = useQuery(ALL_AUTHORS)
+
+  if(result.loading) {
+    return <div>loading...</div>
+  }
+
+  console.log(result.data.allAuthors)
 
   return (
     <div>
