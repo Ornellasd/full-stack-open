c65ed404f75da0bb0599fe7dba197a56801fcab3
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-08-13 13:32:34 -0700

    breakout queries.js

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,20 +1,10 @@
 import React, { useState } from 'react'
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from '../src/queries'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 
-import { gql, useQuery } from '@apollo/client'
-
-const ALL_AUTHORS = gql`
-  query {
-    allAuthors {
-      name
-      id
-      born
-      bookCount
-    }
-  }
-`
 const App = () => {
   const [page, setPage] = useState('authors')
   const result = useQuery(ALL_AUTHORS)
