ab0cdb05bf861ee3cfa62843d33d8f30861b1462
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-08-15 12:50:06 -0700

    initialize excerciseCalculator

---
+++ part9/first_Steps/excerciseCalculator.ts
@@ -0,0 +1,12 @@
+interface Result {
+  periodLength: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (hourArr: Array<string>): Result => {
+
+}
