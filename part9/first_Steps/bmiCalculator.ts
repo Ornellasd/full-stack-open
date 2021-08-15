d25cc237c5358df81bd371b725aea832fdb309c7
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-08-14 23:45:19 -0700

    finish 9.1 - calculateBmi function implemented

--- part9/first_Steps/bmiCalculator.ts
+++ part9/first_Steps/bmiCalculator.ts
@@ -1,6 +1,15 @@
-const calculateBmi = (height: number, weight: number) => {
-  const formula =  weight / ((height / 100) ** 2);
-  console.log(formula);
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi: number =  weight / ((height / 100) ** 2);
+
+  if(bmi < 18.5) {
+    return 'Underweight';
+  } else if(bmi >= 18.5 && bmi < 24.9) {
+    return 'Normal (healthy weight)';
+  } else if(bmi >= 25 && bmi < 29.9) {
+    return 'Overweight';
+  } else {
+    return 'Obese';
+  }
 }
 
-calculateBmi(180, 74);
+console.log(calculateBmi(180, 74));
