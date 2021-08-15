4b56876d051da9fb4fd01d08c7ca2cf2a4e86c1e
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-08-15 13:34:09 -0700

    finish 9.2 - excerciseCalculator features implemented with TypeScript

--- part9/first_Steps/excerciseCalculator.ts
+++ part9/first_Steps/excerciseCalculator.ts
@@ -1,5 +1,6 @@
 interface Result {
   periodLength: number;
+  trainingDays: number;
   success: boolean;
   rating: number;
   ratingDescription: string;
@@ -7,6 +8,39 @@ interface Result {
   average: number;
 }
 
-const calculateExercises = (hourArr: Array<string>): Result => {
+const calculateExercises = (hourArr: Array<number>, target: number): Result => {
+  const average =  hourArr.reduce((sum, value) => {
+      return sum + value;
+    }, 0) / hourArr.length;
 
+  const percentage = average / target;
+
+  let rating: number;
+  let ratingDescription: string;
+
+  if(percentage >= 1) {
+    rating = 3;
+    ratingDescription = 'Perfect! Keep it up.';
+  } else if(percentage > 0.7) {
+    rating = 2;
+    ratingDescription = 'Good job, but you can improve.';
+  } else {
+    rating = 1;
+    ratingDescription = 'Try better next week...';
+  }
+
+  const result = {
+    periodLength: hourArr.length,
+    trainingDays: hourArr.filter(d => d !== 0).length,
+    success: average < target ? false : true,
+    rating,
+    ratingDescription,
+    target,
+    average
+  }
+
+  return result;
 }
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+console.log(calculateExercises([1, 1, 1, 0, 0, 0, 1], 2));
