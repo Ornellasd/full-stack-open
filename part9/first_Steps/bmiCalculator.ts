53dd87db828e4888a4379af8ad75b8c193daf014
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-08-15 16:25:58 -0700

    finish 9.3 - bmiCalculator and excerciseCalculator now use CLI arguments, with error handling

--- part9/first_Steps/bmiCalculator.ts
+++ part9/first_Steps/bmiCalculator.ts
@@ -12,6 +12,28 @@ const calculateBmi = (height: number, weight: number): string => {
   }
 }
 
-const height: number = Number(process.argv[2]);
-const weight: number = Number(process.argv[3]);
-console.log(calculateBmi(height, weight))
+interface BmiValues {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight))
+} catch(e) {
+  console.log('Error, something bad happened, message: ', e.message);
+}
