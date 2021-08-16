2e9ccb26dd846ee0560243f63a39dd99d8d0053a
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-08-16 13:14:37 -0700

    finish 9.7 - endpoints for calculations fully implemented with error handling in express

--- part9/first_Steps/index.ts
+++ part9/first_Steps/index.ts
@@ -9,22 +9,21 @@ app.use(express.json());
 app.get('/bmi', (_req, res) => {
   const { height, weight } = _req.query;
 
-  if((!height || !weight) || (isNaN(Number(height)) || isNaN(Number(weight)))) {
-    throw new Error('malformatted parameters');
-  } else {
-    res.send({
-      weight,
-      height,
-      bmi: calculateBmi(Number(height), Number(weight))
-    });
-  }
+  if(!height || !weight) res.status(400).json({ error: 'missing parameters' });
+  if(isNaN(Number(height)) || isNaN(Number(weight))) res.status(400).json({ error: 'malformatted parameters' });
+
+  res.send({
+    weight,
+    height,
+    bmi: calculateBmi(Number(height), Number(weight))
+  });
 });
 
 app.post('/exercise', (_req, res) => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const { daily_exercises, target } = _req.body;
 
-  if(!daily_exercises || ! target) res.status(400).json({ error: 'missing parameters' });
+  if(!daily_exercises || !target) res.status(400).json({ error: 'missing parameters' });
   if(!Array.isArray(daily_exercises) || isNaN(target)) res.status(400).json({ error: 'malformatted parameters' });
 
   res.send(calculateExercises(daily_exercises, target));
