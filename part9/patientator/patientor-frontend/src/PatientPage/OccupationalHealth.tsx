8bb650d5afa473f752ff75eb57619d2d621cebf6
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-08-23 21:46:42 -0700

    finish 9.21-9.22 - conditional rendering for diagnosis codes and sick leave added

--- part9/patientator/patientor-frontend/src/PatientPage/OccupationalHealth.tsx
+++ part9/patientator/patientor-frontend/src/PatientPage/OccupationalHealth.tsx
@@ -9,18 +9,28 @@ const OccupationalHealthcare = ({ entry, diagnoses }: { entry: OccupationalHealt
     <div key={entry.id} className="ui segment">
       <h2>{entry.date} <Icon name="building" /></h2>
       <em style={{ color: 'grey' }}>{entry.description}</em>
-      <h4>Diagnoses:</h4>
-      <div className="ui bulleted list">
-        {Object.keys(diagnoses).length > 0 && entry.diagnosisCodes?.map(code =>
-          <div key={code} className="item">
-            {filteredDiagnosisName(code, diagnoses)}
+
+      {entry.diagnosisCodes &&
+        <div style={{ paddingTop: '10px' }}>
+          <h4>Diagnoses:</h4>
+          <div className="ui bulleted list">
+            {Object.keys(diagnoses).length > 0 && entry.diagnosisCodes.map(code =>
+              <div key={code} className="item">
+                {filteredDiagnosisName(code, diagnoses)}
+              </div>
+            )}
           </div>
-        )}
-      </div>
-      {entry.sickLeave &&
-        <p>DERP</p>
+        </div>
       }
-      <strong>Sick leave:</strong> {entry.sickLeave?.startDate} to {entry.sickLeave?.endDate}
+    
+      {entry.sickLeave  &&
+        <div style={{ paddingTop: '10px' }}>
+          <strong>
+            Sick leave:
+          </strong>
+          <span> {entry.sickLeave?.startDate} to {entry.sickLeave.endDate}</span>
+        </div>
+      }
     </div>
   );
 };
