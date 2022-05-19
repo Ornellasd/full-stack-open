3b8126dd3ff9223d69ddd6713e3139f48627ad26
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2022-05-18 18:10:59 -0700

    bookCount query by specific author working

--- part8/library-backend/src/index.js
+++ part8/library-backend/src/index.js
@@ -56,26 +56,67 @@ const typeDefs = gql`
   }
 `
 
+let books = [
+  {
+    title: 'Clean Code',
+    published: 2008,
+    author: 'Robert Martin',
+    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring']
+  },
+  {
+    title: 'Agile software development',
+    published: 2002,
+    author: 'Robert Martin',
+    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+    genres: ['agile', 'patterns', 'design']
+  },
+  {
+    title: 'Refactoring, edition 2',
+    published: 2018,
+    author: 'Martin Fowler',
+    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring']
+  },
+  {
+    title: 'Refactoring to patterns',
+    published: 2008,
+    author: 'Joshua Kerievsky',
+    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring', 'patterns']
+  },  
+  {
+    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
+    published: 2012,
+    author: 'Sandi Metz',
+    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring', 'design']
+  },
+  {
+    title: 'Crime and punishment',
+    published: 1866,
+    author: 'Fyodor Dostoevsky',
+    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+    genres: ['classic', 'crime']
+  },
+  {
+    title: 'The Demon ',
+    published: 1872,
+    author: 'Fyodor Dostoevsky',
+    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+    genres: ['classic', 'revolution']
+  },
+];
+
 const resolvers = {
   Query: {
     bookCount: async () => Book.collection.countDocuments(),
     authorCount: async () => Author.collection.countDocuments(),
     allBooks: async (root, args) => {
-      console.log(args, 'arggggs matey')
-      // if(args.genre && args.author) {
-      //   return books.filter(book => book.author === args.author && book.genres.includes(args.genre))
-      // } else if(args.author) {
-      //   return books.filter(book => book.author === args.author)
-      // } else if(args.genre){
-      //   return books.filter(book => book.genres.includes(args.genre))
-      // } else {
-      //   return books
-      // }
       const author = await Author.findOne({ name: args.author })
 
       if(args.genre && args.author) {
-        // return await Book.find({})
-        return await Book.find({ author: author._id, genres: { $in: args.genre} })    
+        return await Book.find({ author: author._id, genres: { $in: args.genre } })    
       } else if(args.author) {
         return await Book.find({ author: author._id }).populate('author')
       } else if(args.genre) {
@@ -87,9 +128,9 @@ const resolvers = {
     allAuthors: async () => await Author.find({}),
   },
   Author: {
-    // bookCount: (root) => {
-    //   return books.filter(book => book.author === root.name).length
-    // }
+    bookCount: async (root) => {
+      return Book.find({ author: root._id }).countDocuments()
+    }
   },
   Mutation: {
     addBook: async (root, args) => {
