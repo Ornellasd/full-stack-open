492a5a7d3b1923698916661723fe4a33e195a8ca
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2022-05-19 19:06:45 -0700

    finish 8.18 - login functionality implemented

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -58,12 +58,13 @@ const App = () => {
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
-        {token &&
-          <button onClick={() => setPage('add')}>add book</button>
-        }
-        <button onClick={() => setPage('login')}>login</button>
-        {token &&
-          <button onClick={logout}>logout</button>
+        {token
+          ? 
+            <>
+              <button onClick={() => setPage('add')}>add book</button>
+              <button onClick={logout}>logout</button>
+            </>
+          : <button onClick={() => setPage('login')}>login</button>
         }
       </div>
 
