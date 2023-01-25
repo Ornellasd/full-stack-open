3aed0b593878d72d0a633842a482bd00e9ed1783
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2023-01-25 12:31:32 +0100

    initialize one to many relationship between user and notes

--- part13/notes_app/backend/controllers/notes.js
+++ part13/notes_app/backend/controllers/notes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-const Note = require('../models/note')
+const { User, Note } = require('../models')
 
 router.get('/', async (req, res) => {
   const notes = await Note.findAll()
@@ -12,10 +12,15 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
+    const user = await User.findOne()
     const date = new Date()
-    const note = await Note.build(req.body)
-    // note.date = date
-    await note.save()
+
+    const note = await Note.create({
+      ...req.body,
+      date,
+      userId: user.id
+    })
+
     return res.json(note)
   } catch (error) {
     return res.status(400).json({ error })
@@ -38,6 +43,16 @@ router.put('/:id', async (req, res) => {
     note.important = req.body.important
     await note.save()
     res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  const note = await Note.findByPk(req.params.id)
+  if (note) {
+    await note.destroy()
+    res.status(200).end()
   } else {
     res.status(404).end()
   }
